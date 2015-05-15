c01b31fd5d0e79eafba69b4f5f3a3122fa8fc0ef
som4ik <somar_melhem@hotmail.com>
2015-05-15 22:17:06 +0300

    modified:   app/models/admin_user.rb 	modified:   app/models/user.rb 	modified:   config/initializers/devise.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,15 +29,16 @@ def self.find_for_database_authentication(warden_conditions)
         where(username: conditions[:username]).first
       end
   end
- protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password,
-      :password_confirmation, :remember_me, :avatar, :avatar_cache,:info_stop) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password,
-      :password_confirmation, :current_password, :avatar, :avatar_cache,:info_stop) }
-  end
 end
+ # protected
+
+#   def configure_permitted_parameters
+#     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password,
+#       :password_confirmation, :remember_me, :avatar, :avatar_cache,:info_stop) }
+#     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password,
+#       :password_confirmation, :current_password, :avatar, :avatar_cache,:info_stop) }
+#   end
+# end
 
 private
   def welcome_message
