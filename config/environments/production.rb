3b8b5a02926510f002acb48031498572fea9af42
som4ik <somar_melhem@hotmail.com>
2015-05-16 19:38:50 +0300

    modified:   config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+ENV["REDISTOGO_URL"] = 'redis://redistogo:771d3454f893beeac6be3a46a735d36c@soapfish.redistogo.com:9640'
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
