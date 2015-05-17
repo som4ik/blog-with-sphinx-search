ee859e49b1eced5a58a57ed1a21629abf5e9b98a
som4ik <somar_melhem@hotmail.com>
2015-05-17 16:28:58 +0300

    modified:   app/indices/post_index.rb 	modified:   app/views/layouts/_header.html.haml

--- app/indices/post_index.rb
+++ app/indices/post_index.rb
@@ -1,7 +1,7 @@
 ThinkingSphinx::Index.define :post, :with => :real_time do
    indexes title, :sortable => true
    indexes body, cached_tag_list
-    indexes user.id,       :as => :user,     :sortable => true
+   indexes user.id,       :as => :user,     :sortable => true
   	 # has confirm,:type => :boolean
   	 # has created_at
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,6 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
   
-
 ENV["REDISTOGO_URL"] = 'redis://username:password@my.host:6389'
 
 
