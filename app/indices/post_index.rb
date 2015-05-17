05364c94c75c6ae613e2354b091c08be3cc414d4
som4ik <somar_melhem@hotmail.com>
2015-05-17 23:48:46 +0300

    modified:   app/indices/post_index.rb 	modified:   app/models/post.rb

--- app/indices/post_index.rb
+++ app/indices/post_index.rb
@@ -1,10 +1,10 @@
-# ThinkingSphinx::Index.define :post, :with => :real_time do
-#    indexes title, :sortable => true
-#    indexes body
-#    has user.id, :type=>:integer,     :as => :user,     :sortable => true
-#    # has confirm,:type => :boolean
-#    has created_at,:type => :timestamp
+ThinkingSphinx::Index.define :post, :with => :real_time do
+   indexes title, :sortable => true
+   indexes body
+   has user.id, :type=>:integer,     :as => :user,     :sortable => true
+   # has confirm,:type => :boolean
+   has created_at,:type => :timestamp
 
 
   
-# end
+end
