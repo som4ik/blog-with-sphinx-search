ef2c8d84197a2dcf4db5aa3c80a333b7e0649e30
som4ik <somar_melhem@hotmail.com>
2015-05-17 22:51:01 +0300

    modified:   config/database.yml 	modified:   config/environments/production.rb 	modified:   config/thinking_sphinx.yml 	modified:   db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-AdminUser.create!([
-  {email: "admin@example.com",password: "password"}
-])
 
 Category.create!([
   {name: "Politices"},
   {name: "Sport"},
   {name: "Art"}
+])
+AdminUser.create!([
+  {email: "admin@example.com",password: "password"}
 ])
