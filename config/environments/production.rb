582817016a0f03271208403883efad5edd192f51
som4ik <somar_melhem@hotmail.com>
2015-05-18 16:17:54 +0300

    modified:   config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
            :enable_starttls_auto => true  
         }
   config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = false
+config.action_mailer.raise_delivery_errors = true
 config.action_mailer.default :charset => "utf-8"
   # Code is not reloaded between requests.
   config.cache_classes = true
