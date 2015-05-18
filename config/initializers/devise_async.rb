e16a8ab98eba42c495649393906e905c7e6420c0
som4ik <somar_melhem@hotmail.com>
2015-05-18 07:57:52 +0300

    modified:   config/initializers/devise.rb 	modified:   config/initializers/devise_async.rb

--- config/initializers/devise_async.rb
+++ config/initializers/devise_async.rb
@@ -1,5 +1,5 @@
 # Supported options: :resque, :sidekiq, :delayed_job, :queue_classic, :torquebox
-Devise::Async.enabled = true
-Devise::Async.backend = :sidekiq
-Devise::Async.queue = :default
+# Devise::Async.enabled = true
+# Devise::Async.backend = :sidekiq
+# Devise::Async.queue = :default
 
